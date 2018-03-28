40b1dea4c8335860a2eca2e3323b02e80dc93b39
madneal <bing.ecnu@gmail.com>
2018-03-28 10:53:36 +0800

    extract GetPageList function :art:

--- web/routers/filterRules.go
+++ web/routers/filterRules.go
@@ -17,35 +17,7 @@ func ListFilterRules(ctx *macaron.Context, sess session.Store) {
 
 	if sess.Get("admin") != nil {
 		rules, pages, _ := models.GetFilterRulesPage(p)
-		pList := 0
-		if (pages - p) > 10 {
-			pList = p + 10
-		} else {
-			pList = pages
-		}
-
-		pageList := make([]int, 0)
-
-		if pages <= 10 {
-			for i:= 1; i < pList; i++ {
-				pageList = append(pageList, i)
-			}
-		} else {
-			if p <= 10 {
-				for i := 1; i <= pList; i++ {
-					pageList = append(pageList, i)
-				}
-			} else {
-				t := p + 5
-				if t > pages {
-					t = pages
-				}
-
-				for i := p - 5; i <= t; i++ {
-					pageList = append(pageList, i)
-				}
-			}
-		}
+		pageList := util.GetPageList(p, pages)
 
 		ctx.Data["pages"] = pages
 		ctx.Data["page"] = page
