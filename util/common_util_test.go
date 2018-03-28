9d27744958e09eab9d9254a37e2560274980a604
madneal <bing.ecnu@gmail.com>
2018-03-28 10:53:36 +0800

    extract GetPageList function :art:

--- util/common_util_test.go
+++ util/common_util_test.go
@@ -7,9 +7,13 @@ import (
 )
 
 func TestGetPageList(t *testing.T) {
-	p := 30
+	//p := 5
+	testPList := []int{1, 5, 10, 23, 45, 100}
 	pages := 100
+	for _, p := range testPList {
+		pageList := util.GetPageList(p, pages)
+		fmt.Printf("p:%d-- the length of pageList:%d  %v\n", p, len(pageList), pageList)
+	}
+	//fmt.Println(pageList)
 	//pageList := make([]int, 0)
-	pageList := util.GetPageList(p, pages)
-	fmt.Println(pageList)
 }
