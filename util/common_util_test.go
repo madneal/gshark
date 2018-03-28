803f1b7fcf2e9d9c3877a9d7e1f28a8994306dbc
madneal <bing.ecnu@gmail.com>
2018-03-28 19:48:02 +0800

    finish navigate to different page :rocket:

--- util/common_util_test.go
+++ util/common_util_test.go
@@ -7,13 +7,10 @@ import (
 )
 
 func TestGetPageList(t *testing.T) {
-	//p := 5
-	testPList := []int{1, 5, 10, 23, 45, 100}
+	testPList := []int{1, 5, 9, 25, 100}
 	pages := 100
 	for _, p := range testPList {
-		pageList := util.GetPageList(p, pages)
+		pageList := util.GetPageList(p, 5, pages)
 		fmt.Printf("p:%d-- the length of pageList:%d  %v\n", p, len(pageList), pageList)
 	}
-	//fmt.Println(pageList)
-	//pageList := make([]int, 0)
 }
