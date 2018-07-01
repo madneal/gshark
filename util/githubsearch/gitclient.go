b67ee0674faa86842aa072e03ab749eb0333a5a4
neal <neal1991@sina.com>
2018-07-01 15:52:39 +0800

    for error condition, return nil and 0 :art:

--- util/githubsearch/gitclient.go
+++ util/githubsearch/gitclient.go
@@ -192,6 +192,7 @@ func searchCodeByOpt(c *Client, ctx context.Context, query string, opt github.Se
 		logger.Log.Infof("remaining: %d, nextPage: %d, lastPage: %d", res.Remaining, res.NextPage, res.LastPage)
 	} else {
 		logger.Log.Infoln(err)
+		return nil, 0
 	}
 	return result, res.NextPage
 }
