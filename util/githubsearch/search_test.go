eeefcab0893eb98730790d62ee1d4fe8bd07d38a
neal1991 <neal1991@sina.com>
2018-08-12 18:32:40 +0800

    add a test for PassFilters :white_check_mark:

--- util/githubsearch/search_test.go
+++ util/githubsearch/search_test.go
@@ -1,1 +1,16 @@
 package githubsearch
+
+import "testing"
+import "../../models"
+import "../githubsearch"
+
+func TestFiltetString(t *testing.T) {
+	codeResult := new(models.CodeResult)
+	id := 1
+	_, _ = models.Engine.Table("code_result").Where("id=?", id).Get(codeResult)
+	if !githubsearch.PassFilters(codeResult) {
+		t.Log("pass the PassFilters function")
+	} else {
+		t.Error("failed to pass the PassFilters function")
+	}
+}
