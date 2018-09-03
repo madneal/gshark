a4890e79a7e225c36066e22b8df731759acafb34
madneal <bing.ecnu@gmail.com>
2018-09-03 21:27:44 +0800

    refactor search and gitclient :art:

--- util/githubsearch/gitclient.go
+++ util/githubsearch/gitclient.go
@@ -25,14 +25,10 @@ THE SOFTWARE.
 package githubsearch
 
 import (
-	"x-patrol/models"
-	"github.com/google/go-github/github"
 	"context"
+	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
-	"x-patrol/logger"
-	"time"
-	"fmt"
-	"strings"
+	"x-patrol/models"
 )
 
 var (
@@ -81,7 +77,7 @@ func GetGithubClient() (*Client, string, error) {
 	return c, c.Token, err
 }
 
-func NewGitClient(GithubClient *github.Client, token string) (*Client) {
+func NewGitClient(GithubClient *github.Client, token string) *Client {
 	return &Client{Client: GithubClient, Token: token}
 }
 
@@ -105,7 +101,7 @@ func (c *Client) GetUserRepos(username string) ([]*github.Repository, *github.Re
 	return c.Client.Repositories.List(ctx, username, nil)
 }
 
-func (c *Client) GetUsersRepos(users []*github.User) (map[string][]*github.Repository) {
+func (c *Client) GetUsersRepos(users []*github.User) map[string][]*github.Repository {
 	result := make(map[string][]*github.Repository)
 	for _, u := range users {
 		repos, resp, _ := c.GetUserRepos(*u.Login)
@@ -115,7 +111,7 @@ func (c *Client) GetUsersRepos(users []*github.User) (map[string][]*github.Repos
 	return result
 }
 
-func (c *Client) GetStrUsersRepos(users []string) (map[string][]*github.Repository) {
+func (c *Client) GetStrUsersRepos(users []string) map[string][]*github.Repository {
 	result := make(map[string][]*github.Repository)
 	for _, u := range users {
 		repos, resp, _ := c.GetUserRepos(u)
@@ -128,71 +124,4 @@ func (c *Client) GetStrUsersRepos(users []string) (map[string][]*github.Reposito
 func (c *Client) GetUserOrgs(username string) ([]*github.Organization, *github.Response, error) {
 	ctx := context.Background()
 	return c.Client.Organizations.List(ctx, username, nil)
-}
-
-func (c *Client) SearchCode(keyword string) ([]*github.CodeSearchResult, error) {
-	var allSearchResult []*github.CodeSearchResult
-	var err error
-	ctx := context.Background()
-	listOpt := github.ListOptions{PerPage: 100}
-	opt := &github.SearchOptions{Sort: "indexed", Order: "desc", TextMatch: true, ListOptions: listOpt}
-	query := keyword + " +in:file"
-	query, err = BuildQuery(query)
-	fmt.Println("search with the query:" + query)
-	for {
-		result, nextPage := searchCodeByOpt(c, ctx, query, *opt)
-		time.Sleep(time.Second * 3)
-		allSearchResult = append(allSearchResult, result)
-		if nextPage <= 0 {
-			break
-		}
-		opt.Page = nextPage
-	}
-	return allSearchResult, err
-}
-
-func BuildQuery(query string) (string, error) {
-	filterRules, err := models.GetFilterRules()
-	str := ""
-	for _, filterRule := range filterRules {
-		ruleValue := filterRule.RuleValue
-		ruleType := filterRule.RuleType
-		ruleKey := filterRule.RuleKey
-		ruleValueList := strings.Split(ruleValue, ",")
-		for _, value := range ruleValueList {
-			if ruleType == 0 {
-				str += " -"
-			} else {
-				str += " +"
-			}
-
-			if ruleKey == "ext" {
-				str += "extension:"
-			} else if ruleKey == "lang" {
-				str += "language:"
-			}
-
-			value = strings.TrimSpace(value)
-			str += value
-		}
-	}
-	builtQuery := query + str
-	return builtQuery, err
-}
-
-func searchCodeByOpt(c *Client, ctx context.Context, query string, opt github.SearchOptions) (*github.CodeSearchResult, int) {
-	result, res, err := c.Client.Search.Code(ctx, query, &opt)
-
-
-	if res != nil && res.Remaining < 10 {
-		time.Sleep(45 * time.Second)
-	}
-
-	if err == nil {
-		logger.Log.Infof("remaining: %d, nextPage: %d, lastPage: %d", res.Remaining, res.NextPage, res.LastPage)
-	} else {
-		logger.Log.Infoln(err)
-		return nil, 0
-	}
-	return result, res.NextPage
 }
