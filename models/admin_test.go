970897c6b295df373d0f58a2ef9002d317222c2c
madneal <bing.ecnu@gmail.com>
2018-09-27 14:49:17 +0800

    add TestAuth :white_check_mark:

---
+++ models/admin_test.go
@@ -0,0 +1,14 @@
+package models
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAuth(t *testing.T) {
+	//fmt.Println(134)
+	NewDbEngine()
+	_, role, _ := Auth("admin", "spdb@admin")
+	assert.Equal(t, "admin", role)
+}
