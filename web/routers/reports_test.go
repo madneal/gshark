61a64b8f74fa79a9a35e6d1920256545e21913b9
neal1991 <neal1991@sina.com>
2018-10-20 10:17:42 +0800

    fmt the project :art:

--- web/routers/reports_test.go
+++ web/routers/reports_test.go
@@ -1,12 +1,12 @@
 package routers
 
 import (
-	"testing"
-	"gshark/models"
 	"fmt"
+	"gshark/models"
+	"testing"
 )
 
-func TestSetUserInfoOfCodeResultDetail(t *testing.T)  {
+func TestSetUserInfoOfCodeResultDetail(t *testing.T) {
 	codeResultDetail, _ := models.GetCodeResultDetailById(int64(1))
 	setUserInfoOfCodeResultDetail(codeResultDetail)
 	fmt.Println(codeResultDetail.Blog)
