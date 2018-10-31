105897e6dc9cbe959c8e978caed938c8697518a8
neal <neal1991@sina.com>
2018-10-31 16:44:23 +0800

    modify the import in common_utils_test.go

--- util/common/common_utils_test.go
+++ util/common/common_utils_test.go
@@ -1,7 +1,6 @@
 package common
 
 import (
-	"./.."
 	"fmt"
 	"testing"
 )
@@ -10,7 +9,7 @@ func TestGetPageList(t *testing.T) {
 	testPList := []int{1, 5, 9, 25, 100}
 	pages := 100
 	for _, p := range testPList {
-		pageList := util.GetPageList(p, 5, pages)
+		pageList := GetPageList(p, 5, pages)
 		fmt.Printf("p:%d-- the length of pageList:%d  %v\n", p, len(pageList), pageList)
 	}
 }
