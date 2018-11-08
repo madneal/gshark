e206e9fa1f41ff2dd6a9fa8ada7e5b1971d1f29e
neal <neal1991@sina.com>
2018-11-08 10:25:15 +0800

    modify position when modify rule by id

--- web/routers/rules.go
+++ web/routers/rules.go
@@ -79,14 +79,14 @@ func DoEditRules(ctx *macaron.Context, sess session.Store) {
 	if sess.Get("admin") != nil {
 		id := ctx.Params(":id")
 		Id, _ := strconv.Atoi(id)
-		part := strings.TrimSpace(ctx.Req.Form.Get("part"))
+		position := strings.TrimSpace(ctx.Req.Form.Get("position"))
 		Type := strings.TrimSpace(ctx.Req.Form.Get("type"))
 		content := strings.TrimSpace(ctx.Req.Form.Get("content"))
 		caption := strings.TrimSpace(ctx.Req.Form.Get("caption"))
 		desc := strings.TrimSpace(ctx.Req.Form.Get("desc"))
 		status := strings.TrimSpace(ctx.Req.Form.Get("status"))
 		intStatus, _ := strconv.Atoi(status)
-		models.EditRuleById(int64(Id), part, Type, content, caption, desc, intStatus)
+		models.EditRuleById(int64(Id), position, Type, content, caption, desc, intStatus)
 		ctx.Redirect("/admin/rules/list/")
 	} else {
 		ctx.Redirect("/admin/login/")
