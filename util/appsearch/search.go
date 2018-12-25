a0900b65100a1edb87682999f2bdf522810adc34
neal <neal1991@sina.com>
2018-12-25 16:54:19 +0800

    add search for app :sparkles:

--- util/appsearch/search.go
+++ util/appsearch/search.go
@@ -5,6 +5,7 @@ import (
 	"github.com/neal1991/gshark/models"
 	"github.com/neal1991/gshark/vars"
 	"time"
+	"github.com/gocolly/colly"
 )
 
 func ScheduleTasks(duration time.Duration) {
@@ -28,4 +29,23 @@ func GenerateSearchCodeTask() (map[int][]models.Rule, error) {
 		result[batch] = rules[vars.SearchNum*batch : ruleNum]
 	}
 	return result, err
+}
+
+func SearchForApp(rule models.Rule)  {
+	appSearchResult := new(models.APPSearchResult)
+	if rule.Caption == "HUAWEI" {
+		url := "http://appstore.huawei.com/search/"
+		url = url + rule.Pattern
+		c := colly.NewCollector()
+		c.OnHTML("body", func(e *colly.HTMLElement) {
+			e.ForEach(".list-game-app", func(i int, element *colly.HTMLElement) {
+				*appSearchResult.Name = element.ChildText(".title")
+				*appSearchResult.Description = element.ChildText(".content")
+				*appSearchResult.DeployDate = element.ChildText("")
+			})
+		})
+		c.Visit(url)
+	} else {
+
+	}
 }
