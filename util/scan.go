7bdc59345f74c2818115f2a2c03efb62ce33e979
neal <neal1991@sina.com>
2018-12-29 10:49:01 +0800

    recover scanMode :sparkles:

--- util/scan.go
+++ util/scan.go
@@ -6,20 +6,38 @@ import (
 	"github.com/urfave/cli"
 
 	"github.com/neal1991/gshark/logger"
-	"github.com/neal1991/gshark/util/appsearch"
 	"time"
+	"github.com/neal1991/gshark/util/appsearch"
+	"strings"
 )
 
 func Scan(ctx *cli.Context) {
+	scanMode := "all"
 	// seconds
 	var Interval time.Duration = 900
+
+	if ctx.IsSet("mode") {
+		scanMode = strings.ToLower(ctx.String("mode"))
+	}
 
 	if ctx.IsSet("time") {
 		Interval = time.Duration(ctx.Int("time"))
 	}
 
-	logger.Log.Println("scan github code ")
-	// use go keyword or not
-	githubsearch.ScheduleTasks(Interval)
-	appsearch.ScheduleTasks(Interval)
+	switch scanMode {
+		case "github":	 	// use go keyword or not
+			logger.Log.Println("scan github code")
+			githubsearch.ScheduleTasks(Interval)
+		case "app":
+			logger.Log.Println("scan app repos")
+			appsearch.ScheduleTasks(Interval)
+		case "all":
+			logger.Log.Println("scan github code and app repos")
+			go githubsearch.ScheduleTasks(Interval)
+			appsearch.ScheduleTasks(Interval)
+		default:
+			logger.Log.Println("scan github code and app repos")
+			go githubsearch.ScheduleTasks(Interval)
+			appsearch.ScheduleTasks(Interval)
+	}
 }
