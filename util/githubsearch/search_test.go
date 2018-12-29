b5ef7616bc79a9d8ec6de328a1fd5e66dbc4ed1b
madneal <bing.ecnu@gmail.com>
2018-12-29 09:28:12 +0800

    add created time and updated time xorm comment for app search result :art:

--- util/githubsearch/search_test.go
+++ util/githubsearch/search_test.go
@@ -7,9 +7,13 @@ func TestFiltetString(t *testing.T) {
 	codeResult := new(models.CodeResult)
 	id := 1
 	_, _ = models.Engine.Table("code_result").Where("id=?", id).Get(codeResult)
-	if !PassFilters(codeResult) {
+	if !PassFilters(codeResult, "abc") {
 		t.Log("pass the PassFilters function")
 	} else {
 		t.Error("failed to pass the PassFilters function")
 	}
+}
+
+func TestScheduleTasks(t *testing.T) {
+	ScheduleTasks(100)
 }
