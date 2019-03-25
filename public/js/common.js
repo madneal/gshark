187fcff302fa9e3a2ebc8bc2e0311c843fee0416
madneal <bing.ecnu@gmail.com>
2019-03-25 11:30:57 +0800

    close #39 finish the app asset new :lipstick:

--- public/js/common.js
+++ public/js/common.js
@@ -8,11 +8,6 @@ $('input[type=radio][name=status]').change(function() {
 	    status = 2;
     }
     window.location.href = window.location.origin + '/admin/reports/github/query/' + status
-    // if (window.location.pathname.indexOf("query") == -1) {
-	 //    window.location.href = window.location.href + "query/" + status;
-    // } else {
-    //     window.location.href = window.location.origin + window.location.pathname.replace(/\d/, status);
-    // }
 })
 var url = window.location;
 // for sidebar menu but not for treeview submenu
@@ -23,3 +18,43 @@ $('ul.sidebar-menu a').filter(function() {
 $('ul.treeview-menu a').filter(function() {
     return this.href == url;
 }).parentsUntil(".sidebar-menu > .treeview-menu").siblings().removeClass('active').end().addClass('active');
+
+document.querySelector('.file .gen-hash').addEventListener('change', function () {
+    var blobSlice = File.prototype.slice || File.prototype.mozSlice || File.prototype.webkitSlice,
+        file = this.files[0],
+        chunkSize = 2097152,                             // Read in chunks of 2MB
+        chunks = Math.ceil(file.size / chunkSize),
+        currentChunk = 0,
+        spark = new SparkMD5.ArrayBuffer(),
+        fileReader = new FileReader();
+
+    fileReader.onload = function (e) {
+        console.log('read chunk nr', currentChunk + 1, 'of', chunks);
+        spark.append(e.target.result);                   // Append array buffer
+        currentChunk++;
+        let hash;
+
+        if (currentChunk < chunks) {
+            loadNext();
+        } else {
+            hash = spark.end();
+            document.querySelector('#hash').value = hash;
+        }
+    };
+
+    fileReader.onerror = function () {
+        console.warn('oops, something went wrong.');
+    };
+
+    function loadNext() {
+        var start = currentChunk * chunkSize,
+            end = ((start + chunkSize) >= file.size) ? file.size : start + chunkSize;
+
+        fileReader.readAsArrayBuffer(blobSlice.call(file, start, end));
+    }
+
+    loadNext();
+});
+
+$('#url').datepicker({
+});
