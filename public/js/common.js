7abd477c1ef5a980025dcad2653891a1a5db99a3
neal1991 <neal1991@sina.com>
2019-09-22 22:06:13 +0800

    modify the size of icon of ignoreall :lipstick:

--- public/js/common.js
+++ public/js/common.js
@@ -19,42 +19,6 @@ $('ul.treeview-menu a').filter(function() {
     return this.href == url;
 }).parentsUntil(".sidebar-menu > .treeview-menu").siblings().removeClass('active').end().addClass('active');
 
-document.querySelector('.file .gen-hash').addEventListener('change', function () {
-    var blobSlice = File.prototype.slice || File.prototype.mozSlice || File.prototype.webkitSlice,
-        file = this.files[0],
-        chunkSize = 2097152,                             // Read in chunks of 2MB
-        chunks = Math.ceil(file.size / chunkSize),
-        currentChunk = 0,
-        spark = new SparkMD5.ArrayBuffer(),
-        fileReader = new FileReader();
-
-    fileReader.onload = function (e) {
-        console.log('read chunk nr', currentChunk + 1, 'of', chunks);
-        spark.append(e.target.result);                   // Append array buffer
-        currentChunk++;
-        let hash;
-
-        if (currentChunk < chunks) {
-            loadNext();
-        } else {
-            hash = spark.end();
-            document.querySelector('#hash').value = hash;
-        }
-    };
-
-    fileReader.onerror = function () {
-        console.warn('oops, something went wrong.');
-    };
-
-    function loadNext() {
-        var start = currentChunk * chunkSize,
-            end = ((start + chunkSize) >= file.size) ? file.size : start + chunkSize;
-
-        fileReader.readAsArrayBuffer(blobSlice.call(file, start, end));
-    }
-
-    loadNext();
-});
 
 $('#deployDate').datepicker({
     autocomplete: true
