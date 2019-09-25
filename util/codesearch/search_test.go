3523d2626f0653d5b53c65ec801c1c577b4dc5ca
madneal <bing.ecnu@gmail.com>
2019-09-25 15:14:34 +0800

    add searchcode feature :tada:

---
+++ util/codesearch/search_test.go
@@ -0,0 +1,20 @@
+package codesearch
+
+import (
+	"github.com/neal1991/gshark/models"
+	"github.com/parnurzeal/gorequest"
+	"testing"
+)
+
+func TestSearchForSearchCode(t *testing.T) {
+	rule := new(models.Rule)
+	rule.Pattern = "SPDB"
+	request := gorequest.New()
+	SearchForSearchCode(*rule, request)
+}
+
+func TestGetResult(t *testing.T) {
+	url :=  "https://searchcode.com/api/codesearch_I/?q=spdb&p=0&per_page=100"
+	request := gorequest.New()
+	GetResult(request, url)
+}
