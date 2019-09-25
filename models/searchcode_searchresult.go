e15882e6e32fa6b1fe03bd6c7bea72acbe721956
madneal <bing.ecnu@gmail.com>
2019-09-25 17:37:34 +0800

    finish search code module :rocket:

--- models/searchcode_searchresult.go
+++ models/searchcode_searchresult.go
@@ -1,19 +1,21 @@
 package models
 
 type SearchCodeRes struct {
-	Previouspage string `json:"previouspage"`
-	Query string `json:"query"`
-	Total int `json:"total"`
-	Page int `json:"page"`
-	Nextpage int `json:"nextpage"`
-	Results []SearchCodeResult `json:"results"`
+	Previouspage string             `json:"previouspage"`
+	Query        string             `json:"query"`
+	Total        int                `json:"total"`
+	Page         int                `json:"page"`
+	Nextpage     int                `json:"nextpage"`
+	Results      []SearchCodeResult `json:"results"`
 }
 
 type SearchCodeResult struct {
-	Repo string
-	Name string
+	Repo     string
+	Name     string
 	Language string
-	Url string
-	Lines map[string]interface{} `json:"lines"`
+	Url      string
+	Location string
+	Lines    map[string]interface{} `json:"lines"`
 	Filename string
+	Id       int
 }
