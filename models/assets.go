aefa6eae82679a51fc209859b69af2cbeefa5177
madneal <bing.ecnu@gmail.com>
2020-02-15 10:46:13 +0800

    modify the struct of asset

--- models/assets.go
+++ models/assets.go
@@ -1,4 +1,12 @@
 package models
 
+import "time"
+
 type Asset struct {
+	Id          int64
+	Owner       string `xorm:"text"`
+	Type        string `xorm:"text notnull"`
+	Status      int
+	CreatedTime time.Time `xorm:"created"`
+	UpdatedTime time.Time `xorm:"updated"`
 }
