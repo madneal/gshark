c8d59629dbe3e9151597acd1596aae62a4c533d1
neal1991 <neal1991@sina.com>
2020-02-16 10:47:11 +0800

    add content

--- models/assets.go
+++ models/assets.go
@@ -6,6 +6,7 @@ type Asset struct {
 	Id          int64
 	Owner       string `xorm:"text"`
 	Type        string `xorm:"text notnull"`
+	Content     string `xorm:"text notnull"`
 	Status      int
 	CreatedTime time.Time `xorm:"created"`
 	UpdatedTime time.Time `xorm:"updated"`
