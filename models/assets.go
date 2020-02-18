e1a1a287434004d22edb4b00d198874560cd7570
neal1991 <neal1991@sina.com>
2020-02-18 13:19:03 +0800

    add asset method

--- models/assets.go
+++ models/assets.go
@@ -4,10 +4,32 @@ import "time"
 
 type Asset struct {
 	Id          int64
-	Owner       string `xorm:"text"`
-	Type        string `xorm:"text notnull"`
+	Org         string `xorm:"text"`
+	AssetType   string `xorm:"text notnull"`
 	Content     string `xorm:"text notnull"`
 	Status      int
 	CreatedTime time.Time `xorm:"created"`
 	UpdatedTime time.Time `xorm:"updated"`
+}
+
+func NewAsset(org, assetType, content string) *Asset {
+	return &Asset{
+		Org:         org,
+		AssetType:   assetType,
+		Content:     content,
+		Status:      0,
+		CreatedTime: time.Time{},
+		UpdatedTime: time.Time{},
+	}
+}
+
+func (asset *Asset) Insert() (int64, error) {
+	return Engine.Insert(asset)
+}
+
+func (asset *Asset) Exists() (bool, error) {
+	return Engine.Exist(&Asset{
+		AssetType: asset.AssetType,
+		Content:   asset.Content,
+	})
 }
