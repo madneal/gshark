f06154a41e8bf871970ac90d40600d049afc71a2
madneal <bing.ecnu@gmail.com>
2020-07-21 13:47:28 +0800

    add md5 generation with specific length

--- misc/md5.go
+++ misc/md5.go
@@ -3,6 +3,7 @@ package misc
 import (
 	"crypto/md5"
 	"fmt"
+	"strings"
 )
 
 func MakeMd5(srcStr string) string {
@@ -13,4 +14,15 @@ func MakeMd5(srcStr string) string {
 
 func GenMd5(srcStr string) string {
 	return fmt.Sprintf("%x", md5.Sum([]byte(srcStr)))
+}
+
+func GenMd5WithSpecificLen(srcStr string, length int) string {
+	hash := GenMd5(srcStr)
+	var result string
+	if len(hash) > length {
+		result = hash[:length]
+	} else {
+		result = hash + strings.Repeat("0", length-len(hash))
+	}
+	return result
 }
