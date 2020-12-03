e159540f4f49c6ab55eb3d5795ddc4986c9b329f
neal1991 <neal1991@sina.com>
2020-12-03 09:40:42 +0800

    use default interval

--- util/scan.go
+++ util/scan.go
@@ -59,11 +59,9 @@ func Scan(ctx *cli.Context) {
 		}
 	default:
 		logger.Log.Println("default scan mode")
-		// hours
-		var dnsInterval time.Duration = 72
 		for {
 			githubsearch.RunTask(Interval)
-			gobuster.RunTask(dnsInterval)
+			gobuster.RunTask(Interval)
 		}
 	}
 }
