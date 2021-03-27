f91e3d2d69b7019f72547ec75604f31e7467d779
madneal <bing.ecnu@gmail.com>
2021-03-27 22:57:59 +0800

    add backend of searchResult

---
+++ gin-vue-admin-master/server/model/searchResult.go
@@ -0,0 +1,24 @@
+// 自动生成模板SearchResult
+package model
+
+import (
+	"gin-vue-admin/global"
+)
+
+// 如果含有time.Time 请自行import time包
+type SearchResult struct {
+      global.GVA_MODEL
+      Repo  string `json:"repo" form:"repo" gorm:"column:repo;comment:;type:varchar(50);size:50;"`
+      Matches  string `json:"matches" form:"matches" gorm:"column:matches;comment:;type:blob;"`
+      Keyword  string `json:"keyword" form:"keyword" gorm:"column:keyword;comment:;type:varchar(100);size:100;"`
+      Path  string `json:"path" form:"path" gorm:"column:path;comment:;type:varchar(100);size:100;"`
+      Url  string `json:"url" form:"url" gorm:"column:url;comment:;type:varchar(500);size:500;"`
+      TextmatchMd5  string `json:"textmatchMd5" form:"textmatchMd5" gorm:"column:textmatch_md5;comment:;type:varchar(100);size:100;"`
+      Status  int `json:"status" form:"status" gorm:"column:status;comment:;type:int;size:3;"`
+}
+
+
+func (SearchResult) TableName() string {
+  return "search_result"
+}
+
