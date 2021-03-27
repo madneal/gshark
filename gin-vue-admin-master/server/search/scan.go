59a982c848849afcdbc288f8668750e11f15093f
madneal <neal1991@sina.com>
2021-03-27 23:14:52 +0800

    ready to add cmd and search module

---
+++ gin-vue-admin-master/server/search/scan.go
@@ -0,0 +1,57 @@
+package search
+
+import (
+	"gin-vue-admin/search/appsearch"
+	"gin-vue-admin/search/codesearch"
+	"gin-vue-admin/search/githubsearch"
+	"gin-vue-admin/search/gitlabsearch"
+	"github.com/urfave/cli"
+	"strings"
+	"time"
+)
+
+func Scan(ctx *cli.Context) {
+	var scanMode string
+	// seconds
+	var Interval time.Duration = 900
+
+	if ctx.IsSet("mode") {
+		scanMode = strings.ToLower(ctx.String("mode"))
+	}
+
+	if ctx.IsSet("time") {
+		Interval = time.Duration(ctx.Int("time"))
+	}
+
+	switch scanMode {
+	case "github":
+		// use go keyword or not
+		for {
+			githubsearch.RunTask(Interval)
+		}
+	case "app":
+		for {
+			appsearch.RunTask(Interval)
+		}
+	case "searchcode":
+		for {
+			codesearch.RunTask(Interval)
+		}
+	case "gitlab":
+		for {
+			gitlabsearch.RunTask(Interval)
+		}
+	case "all":
+		for {
+			gitlabsearch.RunTask(Interval)
+			codesearch.RunTask(Interval)
+			githubsearch.RunTask(Interval)
+			appsearch.RunTask(Interval)
+		}
+	default:
+		for {
+			githubsearch.RunTask(Interval)
+			go gobuster.RunTask(Interval)
+		}
+	}
+}
