cb8200b6600d532f1efa9f9d5aab702511e47763
madneal <neal1991@sina.com>
2021-03-27 20:47:45 +0800

    ready to refactor the whole project

---
+++ gin-vue-admin-master/server/global/global.go
@@ -0,0 +1,19 @@
+package global
+
+import (
+	"go.uber.org/zap"
+
+	"gin-vue-admin/config"
+	"github.com/go-redis/redis"
+	"github.com/spf13/viper"
+	"gorm.io/gorm"
+)
+
+var (
+	GVA_DB     *gorm.DB
+	GVA_REDIS  *redis.Client
+	GVA_CONFIG config.Server
+	GVA_VP     *viper.Viper
+	//GVA_LOG    *oplogging.Logger
+	GVA_LOG    *zap.Logger
+)
