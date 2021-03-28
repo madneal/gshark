0dfdc8a5317bedc7b2b35d6bf6ba07aed1d033c9
madneal <neal1991@sina.com>
2021-03-28 08:37:34 +0800

    modify logo

--- gin-vue-admin-master/web/src/view/login/login.vue
+++ gin-vue-admin-master/web/src/view/login/login.vue
@@ -8,7 +8,7 @@
         <div class="header">
           <a href="/">
             <!-- <img src="~@/assets/logo.png" class="logo" alt="logo" /> -->
-            <span class="title">Gin-Vue-Admin</span>
+            <span class="title">GShark</span>
           </a>
         </div>
       </div>
