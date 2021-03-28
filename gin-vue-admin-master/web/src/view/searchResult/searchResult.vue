8294c7cab1cafdcb18a5e9293db9747a285e7e66
madneal <neal1991@sina.com>
2021-03-28 08:35:38 +0800

    modify button

--- gin-vue-admin-master/web/src/view/searchResult/searchResult.vue
+++ gin-vue-admin-master/web/src/view/searchResult/searchResult.vue
@@ -57,10 +57,10 @@
     
     <el-table-column label="状态" prop="status" width="120"></el-table-column> 
     
-      <el-table-column label="按钮组">
+      <el-table-column label="Review">
         <template slot-scope="scope">
-          <el-button class="table-button" @click="updateSearchResult(scope.row)" size="small" type="primary" icon="el-icon-edit">变更</el-button>
-          <el-button type="danger" icon="el-icon-delete" size="mini" @click="deleteRow(scope.row)">删除</el-button>
+          <el-button class="table-button" @click="updateSearchResult(scope.row)" size="small" type="primary" icon="el-icon-edit">确认</el-button>
+          <el-button type="danger" icon="el-icon-delete" size="mini" @click="deleteRow(scope.row)">忽略</el-button>
         </template>
       </el-table-column>
     </el-table>
