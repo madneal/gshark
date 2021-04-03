89f6d6926a2b27922d776bcc261b78ce247d9bb2
madneal <bing.ecnu@gmail.com>
2021-04-03 18:50:18 +0800

    remove unused folders :fire:

--- utils/md5.go
+++ utils/md5.go
@@ -3,6 +3,8 @@ package utils
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"fmt"
+	"strings"
 )
 
 //@author: [piexlmax](https://github.com/piexlmax)
@@ -15,4 +17,19 @@ func MD5V(str []byte) string {
 	h := md5.New()
 	h.Write(str)
 	return hex.EncodeToString(h.Sum(nil))
+}
+
+func GenMd5(srcStr string) string {
+	return fmt.Sprintf("%x", md5.Sum([]byte(srcStr)))
+}
+
+func GenMd5WithSpecificLen(srcStr string, length int) string {
+	hash := GenMd5(srcStr)
+	var result string
+	if len(hash) > length {
+		result = hash[:length]
+	} else {
+		result = hash + strings.Repeat("0", length-len(hash))
+	}
+	return result
 }
