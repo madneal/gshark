bdc2d284507a91776a8fd0718c47297720af3365
madneal <neal1991@sina.com>
2021-04-05 18:40:11 +0800

    fix for build query

---
+++ server/search/githubsearch/search_test.go
@@ -0,0 +1,21 @@
+package githubsearch
+
+import (
+	"fmt"
+	"github.com/madneal/gshark/core"
+	"github.com/madneal/gshark/global"
+	"github.com/madneal/gshark/initialize"
+	"testing"
+)
+
+func TestBuildQuery(t *testing.T) {
+	//global.GVA_DB = initialize.Gorm()
+	//db, _ := global.GVA_DB.DB()
+	//defer db.Close()
+	global.GVA_VP = core.Viper("/Users/neal/project/gshark/server/config.yaml")      // 初始化Viper
+	global.GVA_LOG = core.Zap()       // 初始化zap日志库
+	//global.GVA_CONFIG.Mysql = mysql
+	global.GVA_DB = initialize.Gorm() // gorm连接数据库
+	q, _ := BuildQuery("spdb")
+	fmt.Println(q)
+}
