27583b91e1679fdbdd408f9a43b46714b35cf38a
madneal <neal1991@sina.com>
2021-04-05 20:19:51 +0800

    rename to gshark

--- web/src/main.js
+++ web/src/main.js
@@ -143,13 +143,3 @@ export default new Vue({
     router,
     store
 }).$mount('#app')
-
-
-console.log(`
-       欢迎使用 Gin-Vue-Admin
-       当前版本:V2.4.0
-       加群方式:微信：shouzi_1994 QQ群：622360840
-       默认自动化文档地址:http://127.0.0.1%s/swagger/index.html
-       默认前端文件运行地址:http://127.0.0.1:8080
-       如果项目让您获得了收益，希望您能请团队喝杯可乐:https://www.gin-vue-admin.com/docs/coffee
-`)
