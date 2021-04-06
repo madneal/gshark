73c2224e897ac958760a17fecb1de66211a28b60
madneal <bing.ecnu@gmail.com>
2021-04-06 21:11:22 +0800

    modify the method of updateSearchResult

--- server/router/searchResult.go
+++ server/router/searchResult.go
@@ -12,7 +12,7 @@ func InitSearchResultRouter(Router *gin.RouterGroup) {
 		SearchResultRouter.POST("createSearchResult", v1.CreateSearchResult)             // 新建SearchResult
 		SearchResultRouter.DELETE("deleteSearchResult", v1.DeleteSearchResult)           // 删除SearchResult
 		SearchResultRouter.DELETE("deleteSearchResultByIds", v1.DeleteSearchResultByIds) // 批量删除SearchResult
-		SearchResultRouter.PUT("updateSearchResult", v1.UpdateSearchResult)              // 更新SearchResult
+		SearchResultRouter.POST("updateSearchResult", v1.UpdateSearchResult)             // 更新SearchResult
 		SearchResultRouter.GET("findSearchResult", v1.FindSearchResult)                  // 根据ID获取SearchResult
 		SearchResultRouter.GET("getSearchResultList", v1.GetSearchResultList)            // 获取SearchResult列表
 	}
