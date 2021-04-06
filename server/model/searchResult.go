7f339aba04c184e53fcf0d4369d6c2b1a8e9fdf0
madneal <neal1991@sina.com>
2021-04-06 08:46:22 +0800

    modify type of TextMatchesJson

--- server/model/searchResult.go
+++ server/model/searchResult.go
@@ -11,15 +11,15 @@ import (
 // 如果含有time.Time 请自行import time包
 type SearchResult struct {
 	global.GVA_MODEL
-	Repo         string             `json:"repo" form:"repo" gorm:"column:repo;comment:;type:varchar(50);size:50;"`
-	RepoUrl   string `gorm:"column:repository;type:varchar(200);"`
-	Matches      string             `json:"matches" form:"matches" gorm:"column:matches;comment:;type:text;"`
-	Keyword      string             `json:"keyword" form:"keyword" gorm:"column:keyword;comment:;type:varchar(100);size:100;"`
-	Path         string             `json:"path" form:"path" gorm:"column:path;comment:;type:varchar(100);size:100;"`
-	Url          string             `json:"url" form:"url" gorm:"column:url;comment:;type:varchar(500);size:500;"`
-	TextmatchMd5 string             `json:"textmatchMd5" gorm:"column:textmatch_md5;comment:;type:varchar(100);size:100;"`
-	Status       int                `json:"status" form:"status" gorm:"column:status;comment:;type:int;size:3;"`
-	TextMatchesJson   datatypes.JSON      `json:"text_matches,omitempty" gorm:"type:json;-"`
+	Repo            string         `json:"repo" form:"repo" gorm:"column:repo;comment:;type:varchar(50);size:50;"`
+	RepoUrl         string         `gorm:"column:repository;type:varchar(200);"`
+	Matches         string         `json:"matches" form:"matches" gorm:"column:matches;comment:;type:text;"`
+	Keyword         string         `json:"keyword" form:"keyword" gorm:"column:keyword;comment:;type:varchar(100);size:100;"`
+	Path            string         `json:"path" form:"path" gorm:"column:path;comment:;type:varchar(100);size:100;"`
+	Url             string         `json:"url" form:"url" gorm:"column:url;comment:;type:varchar(500);size:500;"`
+	TextmatchMd5    string         `json:"textmatchMd5" gorm:"column:textmatch_md5;comment:;type:varchar(100);size:100;"`
+	Status          int            `json:"status" form:"status" gorm:"column:status;comment:;type:int;size:3;"`
+	TextMatchesJson datatypes.JSON `json:"text_matches,omitempty" gorm:"type:json;"`
 }
 
 type TextMatchesJson struct {
