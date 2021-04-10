c3ec666adc366f923ca9f44361727268f1e354fb
madneal <bing.ecnu@gmail.com>
2021-04-10 19:36:27 +0800

    add codesearch

--- server/initialize/router.go
+++ server/initialize/router.go
@@ -37,7 +37,6 @@ func Routers() *gin.Engine {
 		router.InitJwtRouter(PrivateGroup)                   // jwt相关路由
 		router.InitUserRouter(PrivateGroup)                  // 注册用户路由
 		router.InitMenuRouter(PrivateGroup)                  // 注册menu路由
-		router.InitEmailRouter(PrivateGroup)                 // 邮件相关路由
 		router.InitSystemRouter(PrivateGroup)                // system相关路由
 		router.InitCasbinRouter(PrivateGroup)                // 权限相关路由
 		router.InitAutoCodeRouter(PrivateGroup)              // 创建自动化代码
