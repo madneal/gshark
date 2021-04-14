90093d3ce371cf53139595438680fe9256e4df79
madneal <bing.ecnu@gmail.com>
2021-04-14 21:03:00 +0800

    modify initial data type

--- server/model/token.go
+++ server/model/token.go
@@ -12,8 +12,8 @@ type Token struct {
 	Type       string    `json:"type" form:"type" gorm:"column:type;comment:;type:varchar(10);size:10;"`
 	Content    string    `json:"content" form:"content" gorm:"column:content;comment:;type:varchar(100);size:100;"`
 	Desc       string    `json:"desc" form:"desc" gorm:"column:description;comment:;type:varchar(100);size:100;"`
-	LimitTimes int       `json:"limit" form:"limit" gorm:"column:limit_times;comment:;type:int;size:5;"`
-	Remaining  int       `json:"remaining" form:"remaining" gorm:"column:remaining;comment:;type:int;size:5;"`
+	LimitTimes int       `json:"limit" form:"limit" gorm:"column:limit_times;comment:;type:int(5);"`
+	Remaining  int       `json:"remaining" form:"remaining" gorm:"column:remaining;comment:;type:int(5);"`
 	ResetTime  time.Time `json:"resetTime" form:"resetTime" gorm:"column:reset_time;comment:"`
 }
 
