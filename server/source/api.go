e39d9d24c13bd3b61644f363ccb1ab0bf7544563
madneal <neal1991@sina.com>
2021-04-14 08:37:26 +0800

    modify initial data

--- server/source/api.go
+++ server/source/api.go
@@ -81,6 +81,7 @@ var apis = []model.SysApi{
 	{global.GVA_MODEL{ID: 84, CreatedAt: time.Now(), UpdatedAt: time.Now()}, "/excel/downloadTemplate", "下载excel模板", "excel", "GET"},
 	{global.GVA_MODEL{ID: 85, CreatedAt: time.Now(), UpdatedAt: time.Now()}, "/excel/downloadTemplate", "下载excel模板", "excel", "GET"},
 	{global.GVA_MODEL{ID: 86, CreatedAt: time.Now(), UpdatedAt: time.Now()}, "/rule/deleteRule", "删除规则", "rule", "DELETE"},
+	{global.GVA_MODEL{ID: 121, CreatedAt: time.Now(), UpdatedAt: time.Now()}, "/rule/createRule", "新增规则", "rule", "POST"},
 	{global.GVA_MODEL{ID: 87, CreatedAt: time.Now(), UpdatedAt: time.Now()}, "/rule/deleteRuleByIds", "批量删除规则", "rule", "DELETE"},
 	{global.GVA_MODEL{ID: 88, CreatedAt: time.Now(), UpdatedAt: time.Now()}, "/rule/updateRule", "更新规则", "rule", "PUT"},
 	{global.GVA_MODEL{ID: 89, CreatedAt: time.Now(), UpdatedAt: time.Now()}, "/rule/findRule", "根据ID获取规则", "rule", "GET"},
