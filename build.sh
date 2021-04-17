edf1b05c8b3d348cccc3726737db113a930bc92c
madneal <bing.ecnu@gmail.com>
2021-04-17 16:29:37 +0800

    enable cgo

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ cd ../
 
 # build for mac
 cd server
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build 
+GOOS=darwin GOARCH=amd64 go build 
 cd ../releases
 mkdir gshark_darwin_amd64
 cd gshark_darwin_amd64
@@ -18,7 +18,7 @@ cp -rf ./web/dist ./releases/gshark_darwin_amd64
 
 # build for windows
 cd server
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build
+GOOS=windows GOARCH=amd64 go build
 cd ../releases
 mkdir gshark_windows_amd64
 cd gshark_windows_amd64
@@ -31,7 +31,7 @@ cp -rf ./web/dist ./releases/gshark_windows_amd64
 
 # build for linux
 cd server
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o gshark
+GOOS=linux GOARCH=amd64 go build -o gshark
 cd ../releases
 mkdir gshark_linux_amd64
 cd gshark_linux_amd64
