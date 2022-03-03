47f9547156fe9a544dafb677c8961d2b2291afb7
madneal <bing.ecnu@gmail.com>
2022-03-03 21:35:14 +0800

    remove examples module

--- server/model/response/exa_customer.go
+++
@@ -1,7 +0,0 @@
-package response
-
-import "github.com/madneal/gshark/model"
-
-type ExaCustomerResponse struct {
-	Customer model.ExaCustomer `json:"customer"`
-}
--- server/model/response/exa_file_upload_download.go
+++
@@ -1,7 +0,0 @@
-package response
-
-import "github.com/madneal/gshark/model"
-
-type ExaFileResponse struct {
-	File model.ExaFileUploadAndDownload `json:"file"`
-}
