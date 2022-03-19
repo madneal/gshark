d1c3fe337801333a0ff64cb8cc579fc5130ba4c7
madneal <bing.ecnu@gmail.com>
2022-03-19 11:20:18 +0800

    fix for the source-map

--- web/vue.config.js
+++ web/vue.config.js
@@ -17,13 +17,11 @@ module.exports = {
     devServer: {
         port: 8080,
         open: true,
-        overlay: {
-            warnings: false,
-            errors: true
-        },
+        // overlay: {
+        //     warnings: false,
+        //     errors: true
+        // },
         proxy: {
-            // 把key的路径代理到target位置
-            // detail: https://cli.vuejs.org/config/#devserver-proxy
             [process.env.VUE_APP_BASE_API]: { //需要代理的路径   例如 '/api'
                 target: `http://127.0.0.1:8888/`, //代理到 目标路径
                 changeOrigin: true,
@@ -55,7 +53,7 @@ module.exports = {
         config
         // https://webpack.js.org/configuration/devtool/#development
             .when(process.env.NODE_ENV === 'development',
-            config => config.devtool('cheap-source-map')
+            config => config.devtool('eval-source-map')
         )
 
         config
