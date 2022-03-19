91baece8fa9c652eaa6c7cf7892645d13925483d
madneal <bing.ecnu@gmail.com>
2022-03-19 11:23:03 +0800

    remove unnecessary modules

--- web/src/api/rule.js
+++ web/src/api/rule.js
@@ -9,12 +9,12 @@ import service from '@/utils/request'
 // @Success 200 {string} string "{"success":true,"data":{},"msg":"获取成功"}"
 // @Router /rule/createRule [post]
 export const createRule = (data) => {
-     return service({
-         url: "/rule/createRule",
-         method: 'post',
-         data
-     })
- }
+    return service({
+        url: "/rule/createRule",
+        method: 'post',
+        data
+    })
+}
 
 
 // @Tags Rule
@@ -25,13 +25,13 @@ export const createRule = (data) => {
 // @Param data body model.Rule true "删除Rule"
 // @Success 200 {string} string "{"success":true,"data":{},"msg":"删除成功"}"
 // @Router /rule/deleteRule [delete]
- export const deleteRule = (data) => {
-     return service({
-         url: "/rule/deleteRule",
-         method: 'delete',
-         data
-     })
- }
+export const deleteRule = (data) => {
+    return service({
+        url: "/rule/deleteRule",
+        method: 'delete',
+        data
+    })
+}
 
 // @Tags Rule
 // @Summary 删除Rule
@@ -41,13 +41,13 @@ export const createRule = (data) => {
 // @Param data body request.IdsReq true "批量删除Rule"
 // @Success 200 {string} string "{"success":true,"data":{},"msg":"删除成功"}"
 // @Router /rule/deleteRule [delete]
- export const deleteRuleByIds = (data) => {
-     return service({
-         url: "/rule/deleteRuleByIds",
-         method: 'delete',
-         data
-     })
- }
+export const deleteRuleByIds = (data) => {
+    return service({
+        url: "/rule/deleteRuleByIds",
+        method: 'delete',
+        data
+    })
+}
 
 // @Tags Rule
 // @Summary 更新Rule
@@ -57,21 +57,21 @@ export const createRule = (data) => {
 // @Param data body model.Rule true "更新Rule"
 // @Success 200 {string} string "{"success":true,"data":{},"msg":"更新成功"}"
 // @Router /rule/updateRule [put]
- export const updateRule = (data) => {
-     return service({
-         url: "/rule/updateRule",
-         method: 'put',
-         data
-     })
- }
+export const updateRule = (data) => {
+    return service({
+        url: "/rule/updateRule",
+        method: 'put',
+        data
+    })
+}
 
- export const switchRule = (data) => {
-     return service({
-         url: "/rule/switchRuleStatus",
-         method: "post",
-         data
-     })
- }
+export const switchRule = (data) => {
+    return service({
+        url: "/rule/switchRuleStatus",
+        method: "post",
+        data
+    })
+}
 
 
 // @Tags Rule
@@ -82,13 +82,13 @@ export const createRule = (data) => {
 // @Param data body model.Rule true "用id查询Rule"
 // @Success 200 {string} string "{"success":true,"data":{},"msg":"查询成功"}"
 // @Router /rule/findRule [get]
- export const findRule = (params) => {
-     return service({
-         url: "/rule/findRule",
-         method: 'get',
-         params
-     })
- }
+export const findRule = (params) => {
+    return service({
+        url: "/rule/findRule",
+        method: 'get',
+        params
+    })
+}
 
 
 // @Tags Rule
@@ -99,10 +99,10 @@ export const createRule = (data) => {
 // @Param data body request.PageInfo true "分页获取Rule列表"
 // @Success 200 {string} string "{"success":true,"data":{},"msg":"获取成功"}"
 // @Router /rule/getRuleList [get]
- export const getRuleList = (params) => {
-     return service({
-         url: "/rule/getRuleList",
-         method: 'get',
-         params
-     })
- }
+export const getRuleList = (params) => {
+    return service({
+        url: "/rule/getRuleList",
+        method: 'get',
+        params
+    })
+}
