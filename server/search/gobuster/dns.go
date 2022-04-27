057a7951d1cf1b775ce3dd7798e6d0b9501822f1
madneal <bing.ecnu@gmail.com>
2022-04-27 17:24:03 +0800

    add dns module

--- server/search/gobuster/dns.go
+++ server/search/gobuster/dns.go
@@ -2,62 +2,115 @@ package gobuster
 
 import (
 	"bufio"
+	"context"
+	"errors"
 	"fmt"
+	"github.com/google/uuid"
 	"github.com/madneal/gshark/global"
 	"github.com/madneal/gshark/model"
 	"github.com/madneal/gshark/service"
 	"go.uber.org/zap"
-	"os/exec"
+	"net"
+	"os"
 	"strings"
+	"sync"
 	"time"
 )
 
-func RunDNS(domain string) {
-	if global.GVA_CONFIG.Search.GobusterFilePath == "" {
-		fmt.Println("Please specify the file path of gobuster!")
-		return
+const timeout = 30 * time.Second
+
+func preRun(ctx context.Context, domain string) error {
+	guid := uuid.New()
+	_, err := dnsLookup(ctx, fmt.Sprintf("%s.%s", guid, domain))
+	if err == nil {
+		err = errors.New("There may be a wildcard resolve for domain: " + domain)
+		fmt.Println(err)
 	}
-	if global.GVA_CONFIG.Search.SubdomainWordList == "" {
-		fmt.Println("Please specify the file path of subdomain wordlist file path!")
-		return
+	return nil
+}
+
+func getWordlist(wordlistFile string) (*bufio.Scanner, error) {
+	if wordlistFile == "" {
+		return nil, errors.New("has not provide wordlistFile")
 	}
-	cmdLines := global.GVA_CONFIG.Search.GobusterFilePath + " dns -d " + domain + " -w " +
-		global.GVA_CONFIG.Search.SubdomainWordList
-	cmdArgs := strings.Fields(cmdLines)
-	cmd := exec.Command(cmdArgs[0], cmdArgs[1:len(cmdArgs)]...)
-	stdout, err := cmd.StdoutPipe()
-	err = cmd.Start()
+	wordlist, err := os.Open(wordlistFile)
 	if err != nil {
-		global.GVA_LOG.Error("gobuster start error", zap.Any("error", err))
+		return nil, err
 	}
-	oneByte := make([]byte, 100)
-	var foundDomain string
+	return bufio.NewScanner(wordlist), nil
+}
+
+func dnsLookup(ctx context.Context, domain string) ([]string, error) {
+	var resolver net.Resolver
+	return resolver.LookupHost(ctx, domain)
+}
+
+func RunDNS(ctx context.Context, domain string) {
+	if err := preRun(ctx, domain); err != nil {
+		global.GVA_LOG.Error("preRun error", zap.Error(err))
+		return
+	}
+	var workerGroup sync.WaitGroup
+	workerGroup.Add(10)
+	wordChan := make(chan string, 10)
+	for i := 0; i < 10; i++ {
+		go worker(ctx, domain, wordChan, &workerGroup)
+	}
+
+	scanner, err := getWordlist(global.GVA_CONFIG.Search.SubdomainWordList)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+Scan:
+	for scanner.Scan() {
+		select {
+		case <-ctx.Done():
+			break Scan
+		default:
+			word := scanner.Text()
+			wordChan <- word
+		}
+	}
+	close(wordChan)
+	workerGroup.Wait()
+}
+
+func worker(ctx context.Context, domain string, wordChan <-chan string, wg *sync.WaitGroup) {
+	defer wg.Done()
 	for {
-		_, err := stdout.Read(oneByte)
-		if err != nil {
-			global.GVA_LOG.Error("gobuster output error", zap.Any("error", err))
-			break
-		}
-		r := bufio.NewReader(stdout)
-		line, _, _ := r.ReadLine()
-		if strings.Contains(string(line), "Found") {
-			foundDomain = strings.Replace(string(line), "Found: ", "", 1)
-			foundDomain = strings.ToLower(foundDomain)
-			foundDomain = strings.Replace(foundDomain, "\r", "", -1)
-			foundDomain = strings.TrimSpace(foundDomain)
-			println(foundDomain)
-			subdomain := model.Subdomain{
-				Domain:    domain,
-				Subdomain: foundDomain,
-				Status:    0,
+		select {
+		case <-ctx.Done():
+			return
+		case word, ok := <-wordChan:
+			if !ok {
+				return
 			}
-			err := service.CreateSubdomain(subdomain)
+			wordCleaned := strings.TrimSpace(word)
+			err := run(ctx, wordCleaned, domain)
 			if err != nil {
-				global.GVA_LOG.Error("create subdomain failed!", zap.Any("error", err))
+				fmt.Println(err)
 			}
 		}
 	}
-	defer cmd.Wait()
+}
+
+func run(ctx context.Context, word, domain string) error {
+	subdomain := fmt.Sprintf("%s.%s", word, domain)
+
+	_, err := dnsLookup(ctx, subdomain)
+	if err == nil {
+		result := model.Subdomain{
+			Domain:    domain,
+			Subdomain: subdomain,
+			Status:    0,
+		}
+		err = service.CreateSubdomain(result)
+		if err != nil {
+			return err
+		}
+	}
+	return err
 }
 
 func RunTask(duration time.Duration) {
@@ -71,7 +124,7 @@ func RunTask(duration time.Duration) {
 	for _, rule := range rules {
 		start := time.Now()
 		domain := rule.Content
-		RunDNS(domain)
+		RunDNS(context.Background(), domain)
 		fmt.Printf("Complete the scan of domain %s, cost %v, start to sleep %v seconds",
 			domain, time.Since(start), duration*time.Second)
 		time.Sleep(duration * time.Second)
