a3ab3091ec18325269e19787fb3880f17ff5fee4
madneal <bing.ecnu@gmail.com>
2022-05-21 15:21:11 +0800

    improve support for gitlab code search

--- server/search/gitlabsearch/search_test.go
+++ server/search/gitlabsearch/search_test.go
@@ -1,9 +1,11 @@
 package gitlabsearch
 
 import (
+	"fmt"
 	"github.com/madneal/gshark/core"
 	"github.com/madneal/gshark/global"
 	"github.com/madneal/gshark/initialize"
+	"github.com/madneal/gshark/model"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -12,7 +14,6 @@ func TestGetClient(t *testing.T) {
 	initialDataBase()
 	client := GetClient()
 	assert.Equal(t, true, client != nil, "the client is not nil")
-
 }
 
 func initialDataBase() {
@@ -37,4 +38,31 @@ func TestSearchInsideProjects(t *testing.T) {
 	initialDataBase()
 	client := GetClient()
 	SearchInsideProjects("spdb", client)
+}
+
+func TestSearchBlog(t *testing.T) {
+	initialDataBase()
+	//client := GetClient()
+	//SearchBlob(client, "mihoyo")
+}
+
+func TestGetProjectById(t *testing.T) {
+	initialDataBase()
+	client := GetClient()
+	GetProjectById(client, 32123952)
+}
+
+func TestSearchBlobs(t *testing.T) {
+	initialDataBase()
+	client := GetClient()
+	blobs := SearchBlobs(client, "mihoyo")
+	fmt.Println(blobs)
+}
+
+func TestRunSearchTask(t *testing.T) {
+	initialDataBase()
+	rules := []model.Rule{model.Rule{
+		Content: "mihoyo",
+	}}
+	RunSearchTask(&rules)
 }
