a1cf721de76d42c53de5339b073ead1ed795a8ce
madneal <bing.ecnu@gmail.com>
2022-07-02 10:48:13 +0800

    fix for config name

--- server/core/viper.go
+++ server/core/viper.go
@@ -3,7 +3,7 @@ package core
 import (
 	"flag"
 	"fmt"
-	config2 "github.com/madneal/gshark/config"
+	"github.com/madneal/gshark/config"
 	"github.com/madneal/gshark/global"
 	_ "github.com/madneal/gshark/packfile"
 	"os"
@@ -14,36 +14,38 @@ import (
 )
 
 func Viper(path ...string) *viper.Viper {
-	var config string
+	var configFilename string
 	if len(path) == 0 {
-		flag.StringVar(&config, "c", "", "choose config file.")
+		flag.StringVar(&configFilename, "c", "", "choose configFilename file.")
 		flag.Parse()
-		if config == "" { // 优先级: 命令行 > 环境变量 > 默认值
-			if configEnv := os.Getenv(config2.ConfigEnv); configEnv == "" {
-				config = config2.ConfigFile
-				fmt.Printf("您正在使用config的默认值,config的路径为%v\n", config2.ConfigFile)
+		// 优先级: 命令行 > 环境变量 > 默认值
+		if configFilename == "" {
+			if configEnv := os.Getenv(config.ConfigEnv); configEnv == "" {
+				configFilename = config.ConfigFile
+				fmt.Printf("您正在使用config的默认值,config的路径为%v\n", config.ConfigFile)
 			} else {
-				config = configEnv
-				fmt.Printf("您正在使用GVA_CONFIG环境变量,config的路径为%v\n", config)
+				configFilename = configEnv
+				fmt.Printf("您正在使用GVA_CONFIG环境变量,config的路径为%v\n", configFilename)
 			}
 		} else {
-			fmt.Printf("您正在使用命令行的-c参数传递的值,config的路径为%v\n", config)
+			fmt.Printf("您正在使用命令行的-c参数传递的值,config的路径为%v\n", configFilename)
 		}
 	} else {
-		config = path[0]
-		fmt.Printf("您正在使用func Viper()传递的值,config的路径为%v\n", config)
+		configFilename = path[0]
+		fmt.Printf("您正在使用func Viper()传递的值,config的路径为%v\n", configFilename)
 	}
 
 	v := viper.New()
-	v.SetConfigFile(config)
+	v.SetConfigFile(configFilename)
 	err := v.ReadInConfig()
+	fmt.Println(os.Getwd())
 	if err != nil {
-		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+		panic(fmt.Errorf("Fatal error configFilename file: %s \n", err))
 	}
 	v.WatchConfig()
 
 	v.OnConfigChange(func(e fsnotify.Event) {
-		fmt.Println("config file changed:", e.Name)
+		fmt.Println("configFilename file changed:", e.Name)
 		if err := v.Unmarshal(&global.GVA_CONFIG); err != nil {
 			fmt.Println(err)
 		}
