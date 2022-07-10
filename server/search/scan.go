e5bc2538a0a69ab8e2afbe931fe6655865e15465
madneal <bing.ecnu@gmail.com>
2022-07-10 11:35:18 +0800

    remove import

--- server/search/scan.go
+++ server/search/scan.go
@@ -2,13 +2,9 @@ package search
 
 import (
 	"github.com/madneal/gshark/search/codesearch"
-	"github.com/madneal/gshark/search/gitlabsearch"
-
-	//"github.com/madneal/gshark/search/codesearch"
 	"github.com/madneal/gshark/search/githubsearch"
+	"github.com/madneal/gshark/search/gitlabsearch"
 	"github.com/madneal/gshark/search/gobuster"
-
-	//"github.com/madneal/gshark/search/gitlabsearch"
 	"github.com/urfave/cli/v2"
 	"strings"
 	"time"
