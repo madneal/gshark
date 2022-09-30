a3a3c28b1fff92ef09b7206dc98e9c5b68aeea96
madneal <bing.ecnu@gmail.com>
2022-09-30 09:32:39 +0800

    remove comments

--- server/api/v1/sys_email.go
+++ server/api/v1/sys_email.go
@@ -8,12 +8,6 @@ import (
 	"go.uber.org/zap"
 )
 
-// @Tags System
-// @Summary 发送测试邮件
-// @Security ApiKeyAuth
-// @Produce  application/json
-// @Success 200 {string} string "{"success":true,"data":{},"msg":"发送成功"}"
-// @Router /email/emailTest [post]
 func EmailTest(c *gin.Context) {
 	if err := service.EmailTest(); err != nil {
 		global.GVA_LOG.Error("发送失败!", zap.Any("err", err))
