305b84739d996e4e52d53b05e59e82a3b777cfaf
madneal <neal1991@sina.com>
2023-01-08 19:00:07 +0800

    remove logs for no rules

--- server/search/githubsearch/search_test.go
+++ server/search/githubsearch/search_test.go
@@ -13,8 +13,17 @@ func TestBuildQuery(t *testing.T) {
 	global.GVA_VP = core.Viper("/Users/neal/project/gshark/server/config.yaml") // 初始化Viper
 	global.GVA_LOG = core.Zap()                                                 // 初始化zap日志库
 	global.GVA_DB = initialize.Gorm()                                           // gorm连接数据库
-	q, _ := BuildQuery("spdb")
+	q, _ := BuildQuery("/partner_key.*[\"|'\\s]([a-z0-9]{64})[\"|'\\s]/")
 	fmt.Println(q)
+}
+
+func TestClient_SearchCode(t *testing.T) {
+	global.GVA_VP = core.Viper("/Users/neal/project/gshark/server/config.yaml") // 初始化Viper
+	global.GVA_LOG = core.Zap()                                                 // 初始化zap日志库
+	global.GVA_DB = initialize.Gorm()
+	client, _ := GetGithubClient()
+	results, _ := client.SearchCode("/partner_key.*[\"|'\\s]([a-z0-9]{64})[\"|'\\s]/")
+	fmt.Println(results)
 }
 
 func TestClient_GetCommiter(t *testing.T) {
