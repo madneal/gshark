3cefc81d0cbe9763a873cbd6ea4e839edaf42656
madneal <bing.ecnu@gmail.com>
2023-01-13 22:12:30 +0800

    modify filter type

--- server/model/filter.go
+++ server/model/filter.go
@@ -6,13 +6,19 @@ import (
 
 type Filter struct {
 	global.GVA_MODEL
-	Extension string `json:"extension" form:"extension" gorm:"column:extension;comment:;type:varchar(100);"`
-	WhiteExts string `json:"whiteExts" form:"whiteExts" gorm:"column:whiteExts;type:varchar(100);"`
-	Keywords  string `json:"keywords" form:"keywords" gorm:"column:keywords;type:varchar(100);"`
+	FilterType  string `json:"filter_type" form:"filter_type" gorm:"column:filter_type;comment:;type:varchar(20);"`
+	FilterClass string `json:"filter_class" form:"filter_class" gorm:"column:filter_class;type:varchar(20);"`
+	Content     string `json:"content" form:"content" gorm:"column:content;type:varchar(100);"`
 }
 
 func GetFilterRule() (error, Filter) {
 	var filter Filter
 	err := global.GVA_DB.First(&filter).Error
 	return err, filter
+}
+
+func GetFilterByClass(class string) (error, []Filter) {
+	filters := make([]Filter, 0)
+	err := global.GVA_DB.Where("class = ?", class).Find(&filters).Error
+	return err, filters
 }
