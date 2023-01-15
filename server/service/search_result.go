7b2f61f6b1a6d152844e66e8f741359815966785
madneal <neal1991@sina.com>
2023-01-15 22:34:52 +0800

    fix ignore status for repo

--- server/service/search_result.go
+++ server/service/search_result.go
@@ -30,7 +30,7 @@ func UpdateSearchResultByIds(req request.BatchUpdateReq) (err error) {
 
 func IgnoreResultsByRepo(repo string) (err error) {
 	err = global.GVA_DB.Table("search_result").Where("status = 0 and repo = ? and sec_keyword = ''",
-		repo).UpdateColumn("status", 1).Error
+		repo).UpdateColumn("status", global.IgnoredStatus).Error
 	return err
 }
 
