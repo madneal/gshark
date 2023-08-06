138e05505530adb1751479a212abd70b4ce92b8c
madneal <bing.ecnu@gmail.com>
2023-08-06 14:23:39 +0800

    refactor the task initial

--- server/core/server.go
+++ server/core/server.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/madneal/gshark/global"
 	"github.com/madneal/gshark/initialize"
-	"github.com/urfave/cli/v2"
 	"go.uber.org/zap"
 	"time"
 )
@@ -13,17 +12,13 @@ type server interface {
 	ListenAndServe() error
 }
 
-func RunWindowsServer(ctx *cli.Context) error {
+func RunServer() {
 	Router := initialize.Routers()
 	// Router.Static("/form-generator", "./resource/page")
 
 	address := fmt.Sprintf(":%d", global.GVA_CONFIG.System.Addr)
 	s := initServer(address, Router)
-	// 保证文本顺序输出
-	// In order to ensure that the text order output can be deleted
 	time.Sleep(10 * time.Microsecond)
 	global.GVA_LOG.Info("server run success on ", zap.String("address", address))
-
 	global.GVA_LOG.Error(s.ListenAndServe().Error())
-	return nil
 }
--- server/search/scan.go
+++ server/search/scan.go
@@ -6,59 +6,14 @@ import (
 	"github.com/madneal/gshark/search/gitlabsearch"
 	"github.com/madneal/gshark/search/gobuster"
 	"github.com/madneal/gshark/search/postman"
-	"github.com/urfave/cli/v2"
-	"strings"
 	"time"
 )
 
-func Scan(ctx *cli.Context) error {
-	var scanMode string
-	// seconds
+func ScanTask() {
 	var Interval time.Duration = 900
-
-	if ctx.IsSet("mode") {
-		scanMode = strings.ToLower(ctx.String("mode"))
-	}
-
-	if ctx.IsSet("time") {
-		Interval = time.Duration(ctx.Int("time"))
-	}
-
-	switch scanMode {
-	case "github":
-		for {
-			githubsearch.RunTask(Interval)
-		}
-	case "searchcode":
-		for {
-			codesearch.RunTask(Interval)
-		}
-	case "gitlab":
-		for {
-			gitlabsearch.RunTask(Interval)
-		}
-	case "subdomain":
-		for {
-			gobuster.RunTask(Interval)
-		}
-	case "postman":
-		for {
-			postman.RunTask()
-		}
-	case "all":
-		for {
-			gitlabsearch.RunTask(Interval)
-			codesearch.RunTask(Interval)
-			githubsearch.RunTask(Interval)
-			gobuster.RunTask(Interval)
-			postman.RunTask()
-		}
-	default:
-		for {
-			githubsearch.RunTask(Interval)
-			codesearch.RunTask(Interval)
-			gobuster.RunTask(Interval)
-		}
-	}
-	return nil
+	gitlabsearch.RunTask(Interval)
+	codesearch.RunTask(Interval)
+	githubsearch.RunTask(Interval)
+	gobuster.RunTask(Interval)
+	postman.RunTask()
 }
