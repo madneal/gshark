d3be8f208f47a4f0a15ebbceb6b7ab0607317b38
madneal <bing.ecnu@gmail.com>
2024-05-19 15:20:25 +0800

    drop task table design

--- server/search/scan.go
+++ server/search/scan.go
@@ -7,8 +7,6 @@ import (
 	"github.com/madneal/gshark/search/gitlabsearch"
 	"github.com/madneal/gshark/search/gobuster"
 	"github.com/madneal/gshark/search/postman"
-	"github.com/madneal/gshark/service"
-	"go.uber.org/zap"
 	"time"
 )
 
@@ -18,36 +16,10 @@ func ScanTask() {
 			return
 		}
 		var Interval time.Duration = 900
-		if enable, err := service.CheckTaskStatus("gitlab"); enable {
-			gitlabsearch.RunTask(Interval)
-		} else if err != nil {
-			global.GVA_LOG.Error("CheckTaskStatus for gitlab err", zap.Error(err))
-			break
-		}
-		if enable, err := service.CheckTaskStatus("codesearch"); enable {
-			codesearch.RunTask(Interval)
-		} else if err != nil {
-			global.GVA_LOG.Error("CheckTaskStatus for codesearch err", zap.Error(err))
-			break
-		}
-		if enable, err := service.CheckTaskStatus("github"); enable {
-			githubsearch.RunTask(Interval)
-		} else if err != nil {
-			global.GVA_LOG.Error("CheckTaskStatus for github err", zap.Error(err))
-			break
-		}
-		if enable, err := service.CheckTaskStatus("gobuster"); enable {
-			gobuster.RunTask(Interval)
-		} else if err != nil {
-			global.GVA_LOG.Error("CheckTaskStatus for gobuster err", zap.Error(err))
-			break
-		}
-		if enable, err := service.CheckTaskStatus("postman"); enable {
-			postman.RunTask()
-		} else if err != nil {
-			global.GVA_LOG.Error("CheckTaskStatus for postman err", zap.Error(err))
-			break
-		}
+		gitlabsearch.RunTask(Interval)
+		codesearch.RunTask(Interval)
+		githubsearch.RunTask(Interval)
+		gobuster.RunTask(Interval)
+		postman.RunTask()
 	}
-
 }
