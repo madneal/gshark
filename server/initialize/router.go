e5127e5f34053c97bb7370f3736ac8577ad7069b
madneal <bing.ecnu@gmail.com>
2024-09-22 15:11:46 +0800

    remove comment

--- server/initialize/router.go
+++ server/initialize/router.go
@@ -19,9 +19,6 @@ func Routers() *gin.Engine {
 	global.GVA_LOG.Info("use middleware logger")
 	// 跨域
 	//Router.Use(middleware.Cors()) // 如需跨域可以打开
-	//global.GVA_LOG.Info("use middleware cors")
-	//Router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-	//global.GVA_LOG.Info("register swagger handler")
 	// 方便统一添加路由组前缀 多服务器上线使用
 	PublicGroup := Router.Group("")
 	{
