e112354db57403a8e857256119cd3e89b72eed3b
madneal <bing.ecnu@gmail.com>
2025-03-16 11:17:50 +0800

    optimize the layout of system config

--- web/src/view/systemTools/system/system.vue
+++ web/src/view/systemTools/system/system.vue
@@ -3,18 +3,30 @@
     <el-form :model="config" label-width="100px" ref="form" class="system">
       <!--  System start  -->
       <h2>系统配置</h2>
-      <el-form-item label="环境值">
-        <el-input v-model="config.system.env"></el-input>
-      </el-form-item>
-      <el-form-item label="端口值">
-        <el-input v-model.number="config.system.addr"></el-input>
-      </el-form-item>
-      <el-form-item label="数据初始化">
-        <el-checkbox v-model="config.system.needInitData">开启</el-checkbox>
-      </el-form-item>
-      <el-form-item label="多点登录拦截">
-        <el-checkbox v-model="config.system.useMultipoint">开启</el-checkbox>
-      </el-form-item>
+      <el-row>
+        <el-col :lg="12">
+          <el-form-item label="环境值">
+            <el-input v-model="config.system.env"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :lg="12">
+          <el-form-item label="端口值">
+            <el-input v-model.number="config.system.addr"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :lg="12">
+          <el-form-item label="数据初始化">
+            <el-checkbox v-model="config.system.needInitData">开启</el-checkbox>
+          </el-form-item>
+        </el-col>
+        <el-col :lg="12">
+          <el-form-item label="多点登录拦截">
+            <el-checkbox v-model="config.system.useMultipoint">开启</el-checkbox>
+          </el-form-item>
+        </el-col>
+      </el-row>
       <!--  System end  -->
 
       <!--  JWT start  -->
@@ -26,33 +38,65 @@
 
       <!--  Zap start  -->
       <h2>Zap日志配置</h2>
-      <el-form-item label="级别">
-        <el-input v-model.number="config.zap.level"></el-input>
-      </el-form-item>
-      <el-form-item label="输出">
-        <el-input v-model="config.zap.format"></el-input>
-      </el-form-item>
-      <el-form-item label="日志前缀">
-        <el-input v-model="config.zap.prefix"></el-input>
-      </el-form-item>
-      <el-form-item label="日志文件夹">
-        <el-input v-model="config.zap.director"></el-input>
-      </el-form-item>
-      <el-form-item label="软链接名称">
-        <el-input v-model="config.zap.linkName"></el-input>
-      </el-form-item>
-      <el-form-item label="编码级">
-        <el-input v-model="config.zap.encodeLevel"></el-input>
-      </el-form-item>
-      <el-form-item label="栈名">
-        <el-input v-model="config.zap.stacktraceKey"></el-input>
-      </el-form-item>
-      <el-form-item label="显示行">
-        <el-checkbox v-model="config.zap.showLine"></el-checkbox>
-      </el-form-item>
-      <el-form-item label="输出控制台">
-        <el-checkbox v-model="config.zap.logInConsole"></el-checkbox>
-      </el-form-item>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="级别">
+            <el-input v-model.number="config.zap.level"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="输出">
+            <el-input v-model="config.zap.format"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="日志前缀">
+            <el-input v-model="config.zap.prefix"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="日志文件夹">
+            <el-input v-model="config.zap.director"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="软链接名称">
+            <el-input v-model="config.zap.linkName"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="编码级">
+            <el-input v-model="config.zap.encodeLevel"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="栈名">
+            <el-input v-model="config.zap.stacktraceKey"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="显示行">
+            <el-checkbox v-model="config.zap.showLine"></el-checkbox>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <el-row>
+        <el-col :span="24">
+          <el-form-item label="输出控制台">
+            <el-checkbox v-model="config.zap.logInConsole"></el-checkbox>
+          </el-form-item>
+        </el-col>
+      </el-row>
       <!--  Zap end  -->
 
       <h2>企业微信机器人</h2>
@@ -72,31 +116,62 @@
 
       <!--  Email start  -->
       <h2>邮箱配置</h2>
-      <el-form-item label="是否启用">
-        <el-switch
-            v-model="config.email.enable"
-            active-color="#13ce66"
-            inactive-color="#ff4949">
-        </el-switch>
-      </el-form-item>
-      <el-form-item label="接收者邮箱">
-        <el-input v-model="config.email.to" placeholder="可多个，以逗号分隔"></el-input>
-      </el-form-item>
-      <el-form-item label="端口">
-        <el-input v-model.number="config.email.port"></el-input>
-      </el-form-item>
-      <el-form-item label="发送者邮箱">
-        <el-input v-model="config.email.from"></el-input>
-      </el-form-item>
-      <el-form-item label="host">
-        <el-input v-model="config.email.host"></el-input>
-      </el-form-item>
-      <el-form-item label="secret">
-        <el-input v-model="config.email.secret"></el-input>
-      </el-form-item>
-      <el-form-item label="测试邮件">
-        <el-button @click="email">测试邮件</el-button>
-      </el-form-item>
+      <el-row>
+        <el-col :span="24">
+          <el-form-item label="是否启用">
+            <el-switch
+                v-model="config.email.enable"
+                active-color="#13ce66"
+                inactive-color="#ff4949">
+            </el-switch>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="接收者邮箱">
+            <el-input
+                v-model="config.email.to"
+                placeholder="可多个，以逗号分隔">
+            </el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="端口">
+            <el-input
+                v-model.number="config.email.port"
+                placeholder="请输入端口号">
+            </el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="发送者邮箱">
+            <el-input v-model="config.email.from"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="host">
+            <el-input v-model="config.email.host"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="secret">
+            <el-input v-model="config.email.secret"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="测试邮件">
+            <el-button type="primary" @click="email">测试邮件</el-button>
+          </el-form-item>
+        </el-col>
+      </el-row>
       <!--  Email end  -->
 
       <!--  Casbin start  -->
@@ -108,183 +183,91 @@
 
       <!--  Captcha start  -->
       <h2>验证码配置</h2>
-      <el-form-item label="keyLong">
-        <el-input v-model.number="config.captcha.keyLong"></el-input>
-      </el-form-item>
-      <el-form-item label="imgWidth">
-        <el-input v-model.number="config.captcha.imgWidth"></el-input>
-      </el-form-item>
-      <el-form-item label="imgHeight">
-        <el-input v-model.number="config.captcha.imgHeight"></el-input>
-      </el-form-item>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="keyLong">
+            <el-input v-model.number="config.captcha.keyLong"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="imgWidth">
+            <el-input v-model.number="config.captcha.imgWidth"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="imgHeight">
+            <el-input v-model.number="config.captcha.imgHeight"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <!-- Optional: Empty column or additional field -->
+        </el-col>
+      </el-row>
       <!--  Captcha end  -->
 
       <!--  dbType start  -->
-      <template v-if="config.system.dbType == 'mysql'">
         <h2>mysql 数据库配置</h2>
-        <el-form-item label="username">
-          <el-input v-model="config.mysql.username"></el-input>
-        </el-form-item>
-        <el-form-item label="password">
-          <el-input v-model="config.mysql.password"></el-input>
-        </el-form-item>
-        <el-form-item label="path">
-          <el-input v-model="config.mysql.path"></el-input>
-        </el-form-item>
-        <el-form-item label="dbname">
-          <el-input v-model="config.mysql.dbname"></el-input>
-        </el-form-item>
-        <el-form-item label="maxIdleConns">
-          <el-input v-model.number="config.mysql.maxIdleConns"></el-input>
-        </el-form-item>
-        <el-form-item label="maxOpenConns">
-          <el-input v-model.number="config.mysql.maxOpenConns"></el-input>
-        </el-form-item>
-        <el-form-item label="logMode">
-          <el-checkbox v-model="config.mysql.logMode"></el-checkbox>
-        </el-form-item>
-      </template>
-      <template v-if="config.system.dbType == 'sqlite'">
-        <h2>sqlite admin数据库配置</h2>
-        <el-form-item label="path">
-          <el-input v-model="config.mysql.path"></el-input>
-        </el-form-item>
-        <el-form-item label="maxIdleConns">
-          <el-input v-model.number="config.mysql.maxIdleConns"></el-input>
-        </el-form-item>
-        <el-form-item label="maxOpenConns">
-          <el-input v-model.number="config.mysql.maxOpenConns"></el-input>
-        </el-form-item>
-        <el-form-item label="logger">
-          <el-checkbox v-model="config.mysql.logger"></el-checkbox>
-        </el-form-item>
-      </template>
-      <template v-if="config.system.dbType == 'sqlserver'">
-        <h2>sqlserver admin数据库配置</h2>
-        <el-form-item label="username">
-          <el-input v-model="config.sqlserver.username"></el-input>
-        </el-form-item>
-        <el-form-item label="password">
-          <el-input v-model="config.sqlserver.password"></el-input>
-        </el-form-item>
-        <el-form-item label="path">
-          <el-input v-model="config.sqlserver.path"></el-input>
-        </el-form-item>
-        <el-form-item label="dbname">
-          <el-input v-model="config.sqlserver.dbname"></el-input>
-        </el-form-item>
-        <el-form-item label="maxIdleConns">
-          <el-input v-model.number="config.sqlserver.maxIdleConns"></el-input>
-        </el-form-item>
-        <el-form-item label="maxOpenConns">
-          <el-input v-model.number="config.sqlserver.maxOpenConns"></el-input>
-        </el-form-item>
-        <el-form-item label="logger">
-          <el-checkbox v-model="config.sqlserver.logger"></el-checkbox>
-        </el-form-item>
-      </template>
-      <template v-if="config.system.dbType == 'postgresql'">
-        <h2>postgresql admin数据库配置</h2>
-        <el-form-item label="username">
-          <el-input v-model="config.mysql.username"></el-input>
-        </el-form-item>
-        <el-form-item label="password">
-          <el-input v-model="config.mysql.password"></el-input>
-        </el-form-item>
-        <el-form-item label="dbName">
-          <el-input v-model="config.mysql.dbName"></el-input>
-        </el-form-item>
-        <el-form-item label="port">
-          <el-input v-model="config.mysql.port"></el-input>
-        </el-form-item>
-        <el-form-item label="config">
-          <el-input v-model="config.mysql.config"></el-input>
-        </el-form-item>
-        <el-form-item label="maxIdleConns">
-          <el-input v-model.number="config.mysql.maxIdleConns"></el-input>
-        </el-form-item>
-        <el-form-item label="maxOpenConns">
-          <el-input v-model.number="config.mysql.maxOpenConns"></el-input>
-        </el-form-item>
-        <el-form-item label="logger">
-          <el-checkbox v-model="config.mysql.logger"></el-checkbox>
-        </el-form-item>
-        <el-form-item label="prefer-simple-protocol">
-          <el-checkbox v-model="config.mysql.preferSimpleProtocol"></el-checkbox>
-        </el-form-item>
-      </template>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="username">
+            <el-input v-model="config.mysql.username"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="password">
+            <el-input
+                v-model="config.mysql.password"
+                type="password"
+                show-password>
+            </el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="path">
+            <el-input v-model="config.mysql.path"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="dbname">
+            <el-input v-model="config.mysql.dbname"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="maxIdleConns">
+            <el-input v-model.number="config.mysql.maxIdleConns"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="maxOpenConns">
+            <el-input v-model.number="config.mysql.maxOpenConns"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <el-row>
+        <el-col :span="24">
+          <el-form-item label="logMode">
+            <el-checkbox v-model="config.mysql.logMode">启用日志模式</el-checkbox>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
       <!--  dbType end  -->
 
       <!--  ossType start  -->
-      <template v-if="config.system.ossType == 'local'">
         <h2>本地上传配置</h2>
         <el-form-item label="本地文件路径">
           <el-input v-model="config.local.path"></el-input>
         </el-form-item>
-      </template>
-      <template v-if="config.system.ossType == 'qiniu'">
-        <h2>qiniu上传配置</h2>
-        <el-form-item label="存储区域">
-          <el-input v-model="config.qiniu.zone"></el-input>
-        </el-form-item>
-        <el-form-item label="空间名称">
-          <el-input v-model="config.qiniu.bucket"></el-input>
-        </el-form-item>
-        <el-form-item label="CDN加速域名">
-          <el-input v-model="config.qiniu.imgPath"></el-input>
-        </el-form-item>
-        <el-form-item label="是否使用https">
-          <el-checkbox v-model="config.qiniu.imgPath">开启</el-checkbox>
-        </el-form-item>
-        <el-form-item label="accessKey">
-          <el-input v-model="config.qiniu.accessKey"></el-input>
-        </el-form-item>
-        <el-form-item label="secretKey">
-          <el-input v-model="config.qiniu.secretKey"></el-input>
-        </el-form-item>
-        <el-form-item label="上传是否使用CDN上传加速">
-          <el-checkbox v-model="config.qiniu.useCdnDomains">开启</el-checkbox>
-        </el-form-item>
-      </template>
-       <template v-if="config.system.ossType == 'tencent-cos'">
-        <h2>腾讯云COS上传配置</h2>
-        <el-form-item label="bucket">
-          <el-input v-model="config.tencentCOS.bucket"></el-input>
-        </el-form-item>
-        <el-form-item label="region">
-          <el-input v-model="config.tencentCOS.region"></el-input>
-        </el-form-item>
-        <el-form-item label="secretID">
-          <el-input v-model="config.tencentCOS.secretID"></el-input>
-        </el-form-item>
-        <el-form-item label="secretKey">
-          <el-input v-model="config.tencentCOS.secretKey"></el-input>
-        </el-form-item>
-        <el-form-item label="pathPrefix">
-          <el-input v-model="config.tencentCOS.pathPrefix"></el-input>
-        </el-form-item>
-        <el-form-item label="baseURL">
-          <el-input v-model="config.tencentCOS.baseURL"></el-input>
-        </el-form-item>
-      </template>
-       <template v-if="config.system.ossType == 'aliyun-oss'">
-        <h2>阿里云OSS上传配置</h2>
-        <el-form-item label="endpoint">
-          <el-input v-model="config.aliyunOSS.endpoint"></el-input>
-        </el-form-item>
-        <el-form-item label="accessKeyId">
-          <el-input v-model="config.aliyunOSS.accessKeyId"></el-input>
-        </el-form-item>
-        <el-form-item label="accessKeySecret">
-          <el-input v-model="config.aliyunOSS.accessKeySecret"></el-input>
-        </el-form-item>
-        <el-form-item label="bucketName">
-          <el-input v-model="config.aliyunOSS.bucketName"></el-input>
-        </el-form-item>
-        <el-form-item label="bucketUrl">
-          <el-input v-model="config.aliyunOSS.bucketUrl"></el-input>
-        </el-form-item>
-      </template>
       <!--  ossType end  -->
 
       <el-form-item>
@@ -307,10 +290,6 @@ export default {
         jwt: {},
         casbin: {},
         mysql: {},
-        sqlite: {},
-        qiniu: {},
-        tencentCOS:{},
-        aliyunOSS:{},
         captcha: {},
         zap: {},
         local: {},
@@ -375,5 +354,8 @@ export default {
     font-size: 16px;
     box-shadow: -4px 1px 3px 0px #e7e8e8;
   }
+}
+.el-input {
+  width: 50%;
 }
 </style>
